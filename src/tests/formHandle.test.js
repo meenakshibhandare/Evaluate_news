97bdc91ca36aac9fcd15a4fb89d7cfe596606d43
Minakshi bhandare <meenakshibhandare14@gmail.com>
2019-12-23 11:35:13 -0800

    changing usage of require to "import"

--- src/tests/formHandle.test.js
+++ src/tests/formHandle.test.js
@@ -1,9 +1,9 @@
 //testing for formHandler
 //test that get response occurs
-const formUrlTest = require("../client/js/formHandler.js");
+import { updateUI } from '../client/js/formHandler.js';
 
 test("get api testing", done => {
-  formUrlTest.updateUI("/test").then(response => {
+  updateUI("/test").then(response => {
     expect(response.polarity).toBe("positive");
     expect(response.subjectivity).toBe("Objective");
     done();
