8fff2778fe665494d89720dde0a5dae48f38325e
Minakshi bhandare <meenakshibhandare14@gmail.com>
2019-12-31 17:58:10 -0800

    Add files via upload
    Updated file to test function "sentimentDetails ( from formHandler.js)" using jest-mock-fetch

--- src/tests/formHandle.test.js
+++ src/tests/formHandle.test.js
@@ -1,11 +1,21 @@
-//testing for formHandler
-//test that get response occurs
-import { updateUI } from '../client/js/formHandler.js';
 
-test("get api testing", done => {
-  updateUI("/test").then(response => {
-    expect(response.polarity).toBe("positive");
-    expect(response.subjectivity).toBe("Objective");
-    done();
+import {sentimentDetails} from '../client/js/formHandler';
+//import { exportAllDeclaration } from '@babel/types';
+
+
+//Testing function for sentimentDetails() fn 
+//Using fetch-mock to mock the response from the server
+test("testomg sentimentDetails function", () => {
+  beforeEach(()=>{
+    fetch.resetMocks()
+  });
+  fetch.mockResponseOnce(JSON.stringify({ polarity: 'positive' , subjectivity: "objective"}));
+
+  expect.hasAssertions();
+  return sentimentDetails().then(data => {
+    const result = JSON.parse(data);
+    console.log(result);
+    expect(result.polarity).toEqual('positive');
+    expect(result.subjectivity).toEqual('objective');
   });
 });
